c4e22d7c3a00a82ad5d9dcbe5b5fb494441f680b
lucas oliveira <lucas140101@gmail.com>
2022-11-16 00:04:55 -0300

    react-router-dom installed and working

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import { BrowserRouter , Routes, Route, Link } from 'react-router-dom';
-import SignIn from '.components/SignIn';
-import SignOut from '.components/SignOut';
-import JoinPage from '.components/JoinPage';
-import Dashboard from '.components/Dashboard';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Link to='/signin'></Link>
-      <Link to='/signout'></Link>
-      <Link to='/joinpage'></Link>
-      <Link to='/dashboard'></Link>
-      <Routes>
-        <Route path='/signIn'>
-          <SignIn />
-        </Route>
-        <Route path='/signout'>
-          <SignOut />
-        </Route>
-        <Route path='/joinpage'>
-          <JoinPage />
-        </Route>
-        <Route path='/dashboard'>
-          <Dashboard />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <div className='logo'>LOGO</div>
+      <div className='input-box'>
+        <div className='title-input-box'>
+            <p className='title-input'>
+                Faça o Login:
+            </p>
+        </div>
+        <div className='input-div'>
+            <input type='text' id='user-input' className='input' required/>
+            <label htmlFor='user-input' className='label'>Usuário:</label>
+        </div>
+        <div className='input-div'>
+            <input type='text' id='password-input' className='input' required/>
+            <label htmlFor='password-input' className='label'>Senha:</label>
+        </div>
+     </div>
+    </div>
   );
 }
 
